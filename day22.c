2e47951a2990866bd1da83f906dad20540995a5b
Tejas khari <tejaskhari200@gmail.com>
2026-02-22 19:59:51 +0530

    Add files via upload

---
+++ day22.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Define structure for Node
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+// Function to create a new node
+struct Node* createNode(int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// Function to count nodes
+int countNodes(struct Node* head) {
+    int count = 0;
+    struct Node* temp = head;
+    
+    while (temp != NULL) {
+        count++;
+        temp = temp->next;
+    }
+    
+    return count;
+}
+
+int main() {
+    int n, value;
+    struct Node *head = NULL, *temp = NULL, *newNode = NULL;
+
+    // Read number of nodes
+    scanf("%d", &n);
+
+    // Create linked list
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &value);
+        newNode = createNode(value);
+
+        if (head == NULL) {
+            head = newNode;
+            temp = head;
+        } else {
+            temp->next = newNode;
+            temp = newNode;
+        }
+    }
+
+    // Count and print number of nodes
+    printf("%d\n", countNodes(head));
+
+    return 0;
+}
