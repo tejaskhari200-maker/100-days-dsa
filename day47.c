37a9044a0ca92d23f48ad447c1d91731d30bdf73
Tejas khari <tejaskhari200@gmail.com>
2026-03-22 11:06:57 -0700

    Add files via upload

---
+++ day47.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Structure of a node
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+// Create a new node
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+
+// Function to calculate height
+int height(struct Node* root) {
+    if (root == NULL)
+        return 0;
+
+    int leftHeight = height(root->left);
+    int rightHeight = height(root->right);
+
+    return 1 + (leftHeight > rightHeight ? leftHeight : rightHeight);
+}
+
+// Main function
+int main() {
+    /*
+        Example Tree:
+              1
+             / \
+            2   3
+           /
+          4
+    */
+
+    struct Node* root = createNode(1);
+    root->left = createNode(2);
+    root->right = createNode(3);
+    root->left->left = createNode(4);
+
+    printf("Height of tree: %d\n", height(root));
+
+    return 0;
+}
