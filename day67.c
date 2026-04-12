d54d6c84ebd2d28444180e14d226b17701fc9634
Tejas khari <tejaskhari200@gmail.com>
2026-04-12 05:55:34 -0700

    Add files via upload

---
+++ day67.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 100
+
+int graph[MAX][MAX];
+int visited[MAX];
+int stack[MAX];
+int top = -1;
+int n;
+
+// Function to perform DFS
+void dfs(int v) {
+    visited[v] = 1;
+
+    for (int i = 0; i < n; i++) {
+        if (graph[v][i] == 1 && !visited[i]) {
+            dfs(i);
+        }
+    }
+
+    // Push to stack after visiting all neighbors
+    stack[++top] = v;
+}
+
+// Function for Topological Sort
+void topologicalSort() {
+    for (int i = 0; i < n; i++) {
+        if (!visited[i]) {
+            dfs(i);
+        }
+    }
+
+    printf("Topological Order: ");
+    while (top != -1) {
+        printf("%d ", stack[top--]);
+    }
+}
+
+// Main function
+int main() {
+    printf("Enter number of vertices: ");
+    scanf("%d", &n);
+
+    printf("Enter adjacency matrix:\n");
+    for (int i = 0; i < n; i++) {
+        visited[i] = 0;
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &graph[i][j]);
+        }
+    }
+
+    topologicalSort();
+
+    return 0;
+}
