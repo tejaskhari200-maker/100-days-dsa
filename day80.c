57649be736c6f8fbb7244bb99fa61ecb64b93bdb
Tejas khari <tejaskhari200@gmail.com>
2026-04-21 21:52:45 +0530

    Add files via upload

---
+++ day80.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+#define INF 1000000  // A large value to represent infinity
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int dist[n][n];
+
+    // Input matrix
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &dist[i][j]);
+
+            // Convert -1 (no edge) to INF (except diagonal)
+            if (dist[i][j] == -1 && i != j) {
+                dist[i][j] = INF;
+            }
+        }
+    }
+
+    // Floyd-Warshall Algorithm
+    for (int k = 0; k < n; k++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (dist[i][k] + dist[k][j] < dist[i][j]) {
+                    dist[i][j] = dist[i][k] + dist[k][j];
+                }
+            }
+        }
+    }
+
+    // Output result
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (dist[i][j] == INF)
+                printf("-1 ");
+            else
+                printf("%d ", dist[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
